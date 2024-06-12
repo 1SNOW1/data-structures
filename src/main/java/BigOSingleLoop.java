58d904ede6d06933d4ee253410c39d7b3324ab57
enesorhan <enestcp@gmail.com>
2024-06-12 10:09:10 +0300

    nested loop operations

--- src/main/java/BigOSingleLoop.java
+++ src/main/java/BigOSingleLoop.java
@@ -11,10 +11,25 @@ public class BigOSingleLoop {
         // Task 1 Single Loop ........................
         startTime = System.currentTimeMillis();
         for (int i = 0; i < n; i++) {
-            numberOfOperations =+ 1;
+            numberOfOperations += 1;
         }
-
         endTime = System.currentTimeMillis();
+
+        System.out.println("Number of operations: " + numberOfOperations + " in " + (endTime-startTime) + " milliseconds");
+
+        numberOfOperations = 0; //to reset the number of operations to 0
+        // Task 2 Nested Loops........................
+        startTime = System.currentTimeMillis();
+
+        for (int j = 0; j < n; j++) {
+
+                for (int i = 0; i < n; i++) {
+                    numberOfOperations += 1;
+                }
+        }
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Number of operations: " + numberOfOperations + " in " + (endTime-startTime) + " milliseconds");
 
     }
 }
