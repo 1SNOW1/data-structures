3d0559f20971d514400da826901fe4a2baea1449
enesorhan <enestcp@gmail.com>
2024-06-16 22:39:41 +0300

    Counting sort added

---
+++ src/main/java/CountingSort.java
@@ -0,0 +1,39 @@
+public class CountingSort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {2,5,9,8,2,8,7,10,4,3};
+
+
+        countingSort(intArray, 1, 10);
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+    }
+
+    public static void countingSort(int[] input, int min, int max){
+
+        int[] countArray = new int[(max - min) + 1];
+
+        //counting phase
+        for (int i = 0; i < countArray.length; i++ ){
+            countArray[input[i] - min]++; //gives us the index number of i and increments it
+
+        }
+
+        //adding the values back into the input array
+
+        int j = 0;
+        for (int i = min; i <= max; i++){
+            while (countArray[i - min] > 0){
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+    }
+
+
+}
